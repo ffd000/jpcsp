1c4ff00ba2aff99a76c3deec09097d6ea6acaf70
ffd000 <66318502+ffd000@users.noreply.github.com>
2026-04-04 23:04:00 +0300

    Update REShader.java

--- src/jpcsp/graphics/RE/REShader.java
+++ src/jpcsp/graphics/RE/REShader.java
@@ -798,7 +798,7 @@ public class REShader extends BaseRenderingEngineFunction {
 	@Override
 	public int setBones(int count, float[] values) {
 		shaderContext.setNumberBones(count);
-		shaderContext.setBoneMatrix(count, values);
+		//shaderContext.setBoneMatrix(count, values);
         numberOfWeightsForShader = 0;
 		//super.setBones(count, values);
 
